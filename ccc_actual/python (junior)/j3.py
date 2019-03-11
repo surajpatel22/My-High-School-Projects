7b3563ce095679ce35021c618402e3f0fd3a49f9
Suraj Patel <surajpatel22@hotmail.com>
2019-03-10 22:53:31 -0400

    Delete j3.py

--- ccc_actual/python (junior)/j3.py
+++
@@ -1,9 +0,0 @@
-distances = input().split()
-for i in range(len(distances)):
-    distances[i] = int(distances[i])
-
-print (0, distances[0], distances[0] + distances[1], distances[0] + distances[1] + distances[2], distances[0] + distances[2] + distances[3] + distances[1])
-print (distances[0], 0, distances[1], distances[1] + distances[2], distances[1] + distances[2] + distances[3])
-print (distances[0] + distances[1], distances[1], 0, distances[2], distances[2] + distances[3])
-print (distances[0] + distances[1] + distances[2], distances[1] + distances[2], distances[2], 0, distances[3])
-print (distances[0] + distances[1] + distances[2] + distances[3], distances[1] + distances[2] + distances[3], distances[2] + distances[3], distances[3], 0)
